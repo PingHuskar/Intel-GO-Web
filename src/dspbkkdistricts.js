1eed61596e83a7f17d6c43c1a9a0df1808ea296d
PingHuskar <ping47024@icloud.com>
2023-01-07 13:00:56 +0700

    add more regions

---
+++ src/dspbkkdistricts.js
@@ -0,0 +1,367 @@
+// Source
+// https://en.wikipedia.org/wiki/List_of_districts_of_Bangkok
+// Data
+// https://docs.google.com/spreadsheets/d/1GBfT2yif3d7wGWK1ZP7HHovSybWSKsUwIoMNBG3UK38/edit?usp=sharing
+var textMarkersBigs = [{
+    html: 'Bang Bon',
+    latlng: {
+        lat: 13.6592,
+        lng: 100.3991
+    }
+},
+{
+    html: 'Bang Kapi',
+    latlng: {
+        lat: 13.765833,
+        lng: 100.647778
+    }
+},
+{
+    html: 'Bang Khae',
+    latlng: {
+        lat: 13.696111,
+        lng: 100.409444
+    }
+},
+{
+    html: 'Bang Khen',
+    latlng: {
+        lat: 13.873889,
+        lng: 100.596389
+    }
+},
+{
+    html: 'Bang Kho Laem',
+    latlng: {
+        lat: 13.693333,
+        lng: 100.5025
+    }
+},
+{
+    html: 'Bang Khun Thian',
+    latlng: {
+        lat: 13.660833,
+        lng: 100.435833
+    }
+},
+{
+    html: 'Bang Na',
+    latlng: {
+        lat: 13.680081,
+        lng: 100.5918
+    }
+},
+{
+    html: 'Bang Phlat',
+    latlng: {
+        lat: 13.793889,
+        lng: 100.505
+    }
+},
+{
+    html: 'Bang Rak',
+    latlng: {
+        lat: 13.730833,
+        lng: 100.524167
+    }
+},
+{
+    html: 'Bang Sue',
+    latlng: {
+        lat: 13.809722,
+        lng: 100.537222
+    }
+},
+{
+    html: 'Bangkok Noi',
+    latlng: {
+        lat: 13.770867,
+        lng: 100.467933
+    }
+},
+{
+    html: 'Bangkok Yai',
+    latlng: {
+        lat: 13.722778,
+        lng: 100.476389
+    }
+},
+{
+    html: 'Bueng Kum',
+    latlng: {
+        lat: 13.785278,
+        lng: 100.669167
+    }
+},
+{
+    html: 'Chatuchak',
+    latlng: {
+        lat: 13.828611,
+        lng: 100.559722
+    }
+},
+{
+    html: 'Chom Thong',
+    latlng: {
+        lat: 13.677222,
+        lng: 100.484722
+    }
+},
+{
+    html: 'Din Daeng',
+    latlng: {
+        lat: 13.769722,
+        lng: 100.552778
+    }
+},
+{
+    html: 'Don Mueang',
+    latlng: {
+        lat: 13.913611,
+        lng: 100.589722
+    }
+},
+{
+    html: 'Dusit',
+    latlng: {
+        lat: 13.776944,
+        lng: 100.520556
+    }
+},
+{
+    html: 'Huai Khwang',
+    latlng: {
+        lat: 13.776667,
+        lng: 100.579444
+    }
+},
+{
+    html: 'Khan Na Yao',
+    latlng: {
+        lat: 13.8271,
+        lng: 100.6743
+    }
+},
+{
+    html: 'Khlong Sam Wa',
+    latlng: {
+        lat: 13.859722,
+        lng: 100.704167
+    }
+},
+{
+    html: 'Khlong San',
+    latlng: {
+        lat: 13.730278,
+        lng: 100.509722
+    }
+},
+{
+    html: 'Khlong Toei',
+    latlng: {
+        lat: 13.708056,
+        lng: 100.583889
+    }
+},
+{
+    html: 'Lak Si',
+    latlng: {
+        lat: 13.8875,
+        lng: 100.578889
+    }
+},
+{
+    html: 'Lat Krabang',
+    latlng: {
+        lat: 13.722317,
+        lng: 100.759669
+    }
+},
+{
+    html: 'Lat Phrao',
+    latlng: {
+        lat: 13.803611,
+        lng: 100.6075
+    }
+},
+{
+    html: 'Min Buri',
+    latlng: {
+        lat: 13.813889,
+        lng: 100.748056
+    }
+},
+{
+    html: 'Nong Chok',
+    latlng: {
+        lat: 13.855556,
+        lng: 100.8625
+    }
+},
+{
+    html: 'Nong Khaem',
+    latlng: {
+        lat: 13.704722,
+        lng: 100.348889
+    }
+},
+{
+    html: 'Pathum Wan',
+    latlng: {
+        lat: 13.744942,
+        lng: 100.5222
+    }
+},
+{
+    html: 'Phasi Charoen',
+    latlng: {
+        lat: 13.714722,
+        lng: 100.437222
+    }
+},
+{
+    html: 'Phaya Thai',
+    latlng: {
+        lat: 13.78,
+        lng: 100.542778
+    }
+},
+{
+    html: 'Phra Khanong',
+    latlng: {
+        lat: 13.702222,
+        lng: 100.601667
+    }
+},
+{
+    html: 'Phra Nakhon',
+    latlng: {
+        lat: 13.764444,
+        lng: 100.499167
+    }
+},
+{
+    html: 'Pom Prap Sattru Phai',
+    latlng: {
+        lat: 13.758056,
+        lng: 100.513056
+    }
+},
+{
+    html: 'Prawet',
+    latlng: {
+        lat: 13.716944,
+        lng: 100.694444
+    }
+},
+{
+    html: 'Rat Burana',
+    latlng: {
+        lat: 13.682222,
+        lng: 100.505556
+    }
+},
+{
+    html: 'Ratchathewi',
+    latlng: {
+        lat: 13.758889,
+        lng: 100.534444
+    }
+},
+{
+    html: 'Sai Mai',
+    latlng: {
+        lat: 13.919167,
+        lng: 100.645833
+    }
+},
+{
+    html: 'Samphanthawong',
+    latlng: {
+        lat: 13.731389,
+        lng: 100.514167
+    }
+},
+{
+    html: 'Saphan Sung',
+    latlng: {
+        lat: 13.77,
+        lng: 100.684722
+    }
+},
+{
+    html: 'Sathon',
+    latlng: {
+        lat: 13.708056,
+        lng: 100.526389
+    }
+},
+{
+    html: 'Suan Luang',
+    latlng: {
+        lat: 13.730278,
+        lng: 100.651389
+    }
+},
+{
+    html: 'Taling Chan',
+    latlng: {
+        lat: 13.776944,
+        lng: 100.456667
+    }
+},
+{
+    html: 'Thawi Watthana',
+    latlng: {
+        lat: 13.7878,
+        lng: 100.3638
+    }
+},
+{
+    html: 'Thon Buri',
+    latlng: {
+        lat: 13.725,
+        lng: 100.485833
+    }
+},
+{
+    html: 'Thung Khru',
+    latlng: {
+        lat: 13.6472,
+        lng: 100.4958
+    }
+},
+{
+    html: 'Wang Thonglang',
+    latlng: {
+        lat: 13.7864,
+        lng: 100.6087
+    }
+},
+{
+    html: 'Watthana',
+    latlng: {
+        lat: 13.742222,
+        lng: 100.585833
+    }
+},
+{
+    html: 'Yan Nawa',
+    latlng: {
+        lat: 13.696944,
+        lng: 100.543056
+    }
+},
+]
+
+for (let textMarkersBig of textMarkersBigs) {
+var icon = L.divIcon({
+    className: 'parallax-marker label big',
+    html: textMarkersBig.html,
+    iconSize: [200, 36],
+    iconAnchor: [100, 18]
+});
+L.marker(textMarkersBig.latlng, {
+    icon: icon
+}).addTo(map);
+}
--- src/getRegionScoreDetails.js
+++ src/getRegionScoreDetails.js
@@ -8,7 +8,7 @@ const region_th = [
 	{
 		regionName: "AS11-KILO-01",
 		regionVertices: [[13376136, 99189905], [13320053, 100619655], [14786263, 100619655], [14847989, 99189905]],
-		color: FOUR_COLOR[0]
+		color: FOUR_COLOR[1]
 	},
 	{
 		regionName: "AS11-LIMA-14",
@@ -19,12 +19,12 @@ const region_th = [
 		regionName: "AS11-KILO-00",
 		regionVertices: 
 		[[14847989, 99189905], [14786263, 100619655], [16266725, 100619655], [16333984, 99189905]],
-		color: FOUR_COLOR[2]
+		color: FOUR_COLOR[0]
 	},
 	{
 		regionName: "AS11-LIMA-15",
 		regionVertices: [[14786263, 100619655], [14714517, 102071828], [16188538, 102071828], [16266725, 100619655]],
-		color: FOUR_COLOR[3]
+		color: FOUR_COLOR[0]
 	},
 	{
 		regionName: "AS11-LIMA-12",
@@ -34,22 +34,22 @@ const region_th = [
 	{
 		regionName: "AS11-LIMA-13",
 		regionVertices: [[13254873, 102071828], [13180387, 103543691], [14632517, 103543691], [14714517, 102071828]],
-		color: FOUR_COLOR[3]
+		color: FOUR_COLOR[0]
 	},
 	{
 		regionName: "AS11-LIMA-11",
 		regionVertices: [[14632517, 103543691], [14540110, 105032363], [15998432, 105032363], [16099165, 103543691]],
-		color: FOUR_COLOR[1]
+		color: FOUR_COLOR[0]
 	},
 	{
 		regionName: "AS11-LIMA-10",
 		regionVertices: [[14540110, 105032363], [14437230, 106534837], [15886262, 106534837], [15998432, 105032363]],
-		color: FOUR_COLOR[2]
+		color: FOUR_COLOR[0]
 	},
 	{
 		regionName: "AS12-LIMA-14",
 		regionVertices: [[16099165, 103543691], [15998432, 105032363], [17468565, 105032363], [17577425, 103543691]],
-		color: FOUR_COLOR[3]
+		color: FOUR_COLOR[0]
 	},
 	{
 		regionName: "AS12-LIMA-13",
@@ -59,12 +59,12 @@ const region_th = [
 	{
 		regionName: "AS12-LIMA-02",
 		regionVertices: [[17673993, 102071828], [17577425, 103543691], [19064317, 103543691], [19167859, 102071828]],
-		color: FOUR_COLOR[3]
+		color: FOUR_COLOR[0]
 	},
 	{
 		regionName: "AS12-LIMA-01",
 		regionVertices: [[16188538, 102071828], [16099165, 103543691], [17577425, 103543691], [17673993, 102071828]],
-		color: FOUR_COLOR[1]
+		color: FOUR_COLOR[0]
 	},
 	{
 		regionName: "AS12-LIMA-00",
@@ -74,8 +74,105 @@ const region_th = [
 	{
 		regionName: "AS12-KILO-15",
 		regionVertices: [[16333984, 99189905], [16266725, 100619655], [17758460, 100619655], [17831110, 99189905]],
-		color: FOUR_COLOR[1]
+		color: FOUR_COLOR[0]
 	},
+	{
+		regionName: "AS11-KILO-14",
+		regionVertices: 
+		[[11921340, 99189905], [11870979, 100619655], [13320053, 100619655], [13376136, 99189905]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS11-KILO-15",
+		regionVertices: [[10486388, 99189905], [10441798, 100619655], [11870979, 100619655], [11921340, 99189905]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS10-KILO-10",
+		regionVertices: [[9073922, 99189905], [9035120, 100619655], [10441798, 100619655], [10486388, 99189905]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS10-KILO-09",
+		regionVertices: [[7686415, 99189905], [7653391, 100619655], [9035120, 100619655], [9073922, 99189905]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS10-KILO-06",
+		regionVertices: [[6326159, 99189905], [6298875, 100619655], [7653391, 100619655], [7686415, 99189905]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS10-LIMA-09",
+		regionVertices: [[6298875, 100619655], [6267176, 102071828], [7615023, 102071828], [7653391, 100619655]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS10-KILO-08",
+		regionVertices: [[7714252, 97785140], [7686415, 99189905], [9073922, 99189905], [9106627, 97785140]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS10-KILO-11",
+		regionVertices: [[9106627, 97785140], [9073922, 99189905], [10486388, 99189905], [10523970, 97785140]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS11-KILO-12",
+		regionVertices: [[10523970, 97785140], [10486388, 99189905], [11921340, 99189905], [11963782, 97785140]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS11-LIMA-01",
+		regionVertices: [[11870979, 100619655], [11812455, 102071828], [13254873, 102071828], [13320053, 100619655]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS11-LIMA-02",
+		regionVertices: [[11812455, 102071828], [11745583, 103543691], [13180387, 103543691], [13254873, 102071828]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS12-KILO-14",
+		regionVertices: [[16390650, 97785140], [16333984, 99189905], [17831110, 99189905], [17892312, 97785140]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS11-KILO-02",
+		regionVertices: [[13423396, 97785140], [13376136, 99189905], [14847989, 99189905], [14899999, 97785140]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS11-KILO-03",
+		regionVertices: [[14899999, 97785140], [14847989, 99189905], [16333984, 99189905], [16390650, 97785140]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS12-KILO-13",
+		regionVertices: [[17892312, 97785140], [17831110, 99189905], [19336283, 99189905], [19401876, 97785140]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS12-KILO-08",
+		regionVertices: [[19401876, 97785140], [19336283, 99189905], [20846381, 99189905], [20916196, 97785140]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS12-KILO-11",
+		regionVertices: [[19336283, 99189905], [19258411, 100619655], [20763484, 100619655], [20846381, 99189905]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS12-KILO-12",
+		regionVertices: [[17831110, 99189905], [17758460, 100619655], [19258411, 100619655], [19336283, 99189905]],
+		color: FOUR_COLOR[0]
+	},
+	{
+		regionName: "AS12-LIMA-03",
+		regionVertices: [[17758460, 100619655], [17673993, 102071828], [19167859, 102071828], [19258411, 100619655]],
+		color: FOUR_COLOR[0]
+	},
+	
 ]
 // L.polygon().addTo(map)
 const regionVerticesToPloygon = (d) => {
