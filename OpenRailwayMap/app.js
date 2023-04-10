7a47b4efaf617f1a292e9866f0152fde5e8cd953
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-04-10 22:22:31 +0700

    add polylineMeasure

--- OpenRailwayMap/app.js
+++ OpenRailwayMap/app.js
@@ -21,6 +21,7 @@ const map = L.map(`mapdiv`, {
     // maxZoom:14
     attributionControl: false
 })
+L.control.polylineMeasure().addTo(map)
 var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
