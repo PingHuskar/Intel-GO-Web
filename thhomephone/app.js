d33a28922179c114b6f494996088801e1e685373
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-09-28 22:33:41 +0700

    add thhomephone

---
+++ thhomephone/app.js
@@ -0,0 +1,118 @@
+const searchParam = new URLSearchParams(location.search);
+const LOGDOMAPAPIKEY = localStorage.getItem(`longdokey`);
+const p = searchParam.get(`p`)
+const plotShape = (geocode) => {
+  const color = `black`;
+  axios
+    .get(
+      `https://api.longdo.com/map/services/object?mode=geojson&id=${geocode}&dataset=IG&key=${LOGDOMAPAPIKEY}`
+    )
+    .then((res) => {
+      console.log(res);
+      return res.data.features.at(0).geometry.coordinates;
+    })
+    .then((shapes) => {
+      // console.log(shapes)
+      for (let shape of shapes) {
+        // console.log(shape)
+        if (shape.length > 1) {
+          let newArr = [];
+          for (let mark of shape) {
+            newArr.push([mark.at(1), mark.at(0)]);
+          }
+          L.polygon(newArr, {
+            color: color,
+            fillColor: color,
+          }).addTo(map);
+        } else if (shape.length === 1) {
+          let newArr = [];
+          for (let mark of shape.at(0)) {
+            newArr.push([mark.at(1), mark.at(0)]);
+          }
+          L.polygon(newArr, {
+            color: color,
+            fillColor: color,
+          }).addTo(map);
+        }
+      }
+    });
+};
+
+const map = L.map("map").setView([13.744, 100.533142], 5);
+
+const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+
+function style(feature) {
+  console.log(getColor(feature.popdensity));
+  return {
+    weight: 2,
+    opacity: 1,
+    color: "white",
+    dashArray: "3",
+    fillOpacity: 0.7,
+    fillColor: getColor(feature.popdensity),
+  };
+}
+
+function highlightFeature(e) {
+  const layer = e.target;
+
+  layer.setStyle({
+    weight: 5,
+    color: "#666",
+    dashArray: "",
+    fillOpacity: 0.7,
+  });
+
+  layer.bringToFront();
+
+  info.update(layer.feature.properties);
+}
+
+/* global statesData */
+
+if (!p) {
+  alert(`Invalid p`)
+} else if (!LOGDOMAPAPIKEY) {
+  alert(`LOGDOMAPAPIKEY not set`)
+} else {
+  axios.get(`data.json`)
+  .then(res => res.data)
+  .then(data => {
+    const telno = p
+    const telno2 = telno.slice(0, 2)
+    const zones = data.find(i => i.pre == telno2).zones
+    console.log(zones)
+    const zone = zones.find(zone => zone.pres == telno.slice(0, 3)).pro_code
+    zone.forEach(plotShape)
+  })
+  .catch(e => {
+    alert(e)
+  })
+}
+// const geojson = L.geoJson(data, {
+//   style,
+//   onEachFeature,
+// }).addTo(map);
+
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+  info.update();
+}
+
+function zoomToFeature(e) {
+  map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature,
+  });
+}
