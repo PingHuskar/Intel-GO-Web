dcd734dc756d8a740d40de588819e3c1a3b3e922
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-10-26 22:48:24 +0700

    .

--- ingress/bkkanomaly2023/app.js
+++ ingress/bkkanomaly2023/app.js
@@ -32,6 +32,12 @@ const gameEntitiesToMap = (gameEntities) => {
             let resonator_nums = detail.at(6)
             let picurl = detail.at(7)
             let name = detail.at(8)
+            console.log(`{
+                c: ${c},
+                name: \`${name}\`,
+                geo: [${lat},${lng}],
+                picurl: \`${picurl}\`,
+            },`)
             // markers.addLayer(
             L.marker([lat,lng])
             .bindPopup(`<h2>${name}</h2>
@@ -56,15 +62,38 @@ const gameEntitiesToMap = (gameEntities) => {
 }
 
 
-axios.get(`data.json`)
-.then(res => res.data.result.map)
-.then(map => {
-    // console.log(map)
-    for (const [key, value] of Object.entries(map)) {
-        gameEntitiesToMap(map[key].gameEntities)
-    }
+// axios.get(`data.json`)
+// .then(res => res.data.result.map)
+// .then(map => {
+//     // console.log(map)
+//     for (const [key, value] of Object.entries(map)) {
+//         gameEntitiesToMap(map[key].gameEntities)
+//     }
     
-})
-.then(() => {
-    // map.addLayer(markers)
+// })
+// .then(() => {
+//     // map.addLayer(markers)
+// })
+
+const portallist = document.querySelector(`#portallist`)
+
+axios.get(`clean.json`)
+.then(res => res.data)
+.then(portals => {
+    for (let portal of portals) {
+        L.marker(portal.geo)
+            .bindPopup(`<h2>${portal.name}</h2>
+            <img src="${portal.picurl}" />
+            <img src="../../src/images/googlemaps.png" onclick="window.open('https://www.google.com/maps?daddr=${portal.geo.at(0)},${portal.geo.at(1)}', '_blank')">
+            `).bindTooltip(`${portal.name}`).openTooltip()
+            .addTo(map)
+        L.donut(portal.geo, {
+            radius: ACTIONRADIUS,
+            innerRadius: 0,
+            innerRadiusAsPercent: false,
+        }).addTo(map);
+        portallist.innerHTML += `<li onclick="map.flyTo([${portal.geo}],18);">
+        ${portal.name}
+        </li>`
+    }
 })
